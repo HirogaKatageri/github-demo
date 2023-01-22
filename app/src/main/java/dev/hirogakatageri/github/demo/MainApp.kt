63cfbf27ed063b1a9389adbea743575a0ee83676
Gian Quintana <iam@hirogakatageri.dev>
2023-01-22 18:32:09 +0800

    Update manifest and main application file

---
+++ app/src/main/java/dev/hirogakatageri/github/demo/MainApp.kt
@@ -0,0 +1,31 @@
+package dev.hirogakatageri.github.demo
+
+import android.app.Application
+import dev.hirogakatageri.github.demo.data.ServiceProvider
+import dev.hirogakatageri.github.demo.data.service.GithubService
+import dev.hirogakatageri.github.demo.domain.users.UsersRepository
+import dev.hirogakatageri.github.demo.ui.main.MainViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class MainApp : Application() {
+
+    private val appModule = module {
+        single<ServiceProvider> { ServiceProvider() }
+        single<GithubService> { get<ServiceProvider>().githubService }
+        single<UsersRepository> { UsersRepository(get()) }
+
+        viewModel { MainViewModel(get()) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MainApp)
+            modules(appModule)
+        }
+    }
+}
